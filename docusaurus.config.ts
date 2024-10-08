7d74b91d2410c3cf84b022d45d1eca7182f5ea45
unselles <pumape2@seznam.cz>
2024-10-08 03:20:04 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://5kmodding.scp',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'unselles/SCP5K_SDK_Site/',
+  baseUrl: '/SCP5K_SDK_Site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
